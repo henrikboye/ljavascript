0aed708f951144a80ec72ffb706184c778ceab53
henrikboye <henrik.mortensen@gmail.com>
2018-09-18 23:37:29 +0200

    cap 3

--- cap3/main.js
+++ cap3/main.js
@@ -1,1 +1,65 @@
-// var - let - const
+// // var - let - const
+
+// let room1 = "conference_room_a";
+
+// let currentRoom = room1;
+
+// //console.log(currentRoom);
+
+// //let currentRoom = conference_room_a;
+
+// console.log(room1);
+// console.log(currentRoom);
+
+// let str = 'hello';
+// str = 'world';
+// console.log(str);
+
+// let multiline = 'line1\
+// line2';
+
+// multiline = 'line1\n\
+// line2';,
+
+
+// multiline = `line1
+// line2
+// line3
+// `;
+
+
+// console.log(multiline);
+
+// const obj = {};
+
+// obj.color = 'yellow';
+
+// // obj['not an identifier'] = 3;
+// // obj['not an identifier'];
+// // obj['color'];
+
+// const SIZE = Symbol();
+// obj[SIZE] =8;
+// obj[SIZE];
+
+const sam1 = {
+  name: 'Sam',
+  age: 4,
+};
+
+const sam2 = { name: 'Sam', age: 4};
+
+const sam3 = {
+  name: 'Sam',
+  classification :{
+    phylum: 'Chordata',
+    class: 'Mamalia', 
+    order: 'Carnivoria', 
+    family: 'Felidae', 
+    subfamily: 'Felinae', 
+    genus: 'Felis', 
+    species: 'catus'
+  }
+};
+
+console.log(obj);
